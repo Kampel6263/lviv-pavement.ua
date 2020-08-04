72b09628969aff7bb5a93fc7937af3623bd1e384
Kampel6263 <andrian6263@gmail.com>
2020-08-04 22:53:35 +0300

    Delete auto-login.js

--- js.app/auto-login.js
+++
@@ -1,7 +0,0 @@
-var background = document.getElementById('backGround');
-var videoFon = document.getElementById('videoFon');
-var login = document.getElementById('login');
-
-login.style.display = "none";
-background.style.display = "flex";
-videoFon.style.display = "none";
