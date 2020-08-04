73b19552f78d5570d2f6becbf291b99f545d6516
Kampel6263 <andrian6263@gmail.com>
2020-08-04 23:01:00 +0300

    Add files via upload

---
+++ js.app/data-change.js
@@ -0,0 +1,13 @@
+function SetColor(id){
+    let gr = eval.id;
+    gr = document.getElementById(id);
+    square = eval(id);
+    square.style.backgroundColor="#111"
+}
+
+function DataChange(id){
+    let gr = eval.id;
+    gr = document.getElementById(id);
+    square = eval(id);
+    square.style.backgroundColor="#444";
+}
